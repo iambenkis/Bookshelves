1153ec5d3bdbccb350ec272b66ec7157c99753fd
benjamin kisenge <benkisenge03@gmail.com>
2022-10-12 12:05:52 +0200

    Import  in the main file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
