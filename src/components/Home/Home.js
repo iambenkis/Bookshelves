5facfc529592810232f0fd641b35d45bc80c9640
benjamin kisenge <benkisenge03@gmail.com>
2022-10-13 15:11:44 +0200

    Update the UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import { AiOutlineMenuFold, AiOutlineSetting  } from 'react-icons/ai';
-import { BiMicrophone } from 'react-icons/bi'
+import { BiMicrophone } from 'react-icons/bi';
+import Books from '../Books/Books';
+
 
 const Home = () => {
     return (
         <div className='container'>
             <div className='header'>
-                <AiOutlineMenuFold />
-                <h3>My books</h3>
-                <div className='aside-head'>
+                <div className='left-side'>
+                    <AiOutlineMenuFold />
+                    <h3>My books</h3>
+                </div>
+                <div className='right-side'>
                     <BiMicrophone />
                     <AiOutlineSetting />
                 </div>
             </div>
+            <img
+            src='https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+            style={
+                {width: '100%'}
+            }/>
+            <div className='band'>
+                BOOK WITH STARS AND PRICE
+            </div>
+            <Books />
         </div>
     );
 }
