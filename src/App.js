e89a46ca366e5ffc856df79d826a7ebdf1a6a48c
benjamin kisenge <benkisenge03@gmail.com>
2022-10-13 21:55:16 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -10,27 +10,6 @@ import axios from 'axios';
 
 
 const App = () => {
-    // const options = {
-    //     method: 'GET',
-    //     url: 'https://bookshelves.p.rapidapi.com/books',
-    //     headers: {
-    //       'X-RapidAPI-Key': '19e9b2a0fbmsh7c8eba53d77664fp1299bdjsnf9911bd557e8',
-    //       'X-RapidAPI-Host': 'bookshelves.p.rapidapi.com'
-    //     }
-    //   };
-
-    //   axios.request({
-    //   method: 'GET',
-    //   url: 'https://bookshelves.p.rapidapi.com/books',
-    //   headers: {
-    //     'X-RapidAPI-Key': '19e9b2a0fbmsh7c8eba53d77664fp1299bdjsnf9911bd557e8',
-    //     'X-RapidAPI-Host': 'bookshelves.p.rapidapi.com'
-    //   }
-    // }).then(function (response) {
-    //       console.log(response.data);
-    //   }).catch(function (error) {
-    //       console.error(error);
-    //   });
       return (
         <>
             <Home />
