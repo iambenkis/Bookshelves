f5000e75453d7e40f75868f27bc9a31e1bb881e9
benjamin kisenge <benkisenge03@gmail.com>
2022-10-13 21:56:51 +0200

    Add stars to the UI

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,19 +1,47 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchBooks } from "../../redux/bookReducer/reducer";
+import { AiOutlineStar } from 'react-icons/ai';
+import { FaChevronCircleRight } from 'react-icons/fa';
+import './Books.css'
+import StarRating from "../StartRating/StartRating";
 
 const Books = () => {
     const bookStore = useSelector((state) => state.books);
     const dispatch = useDispatch();
-
-    // dispatch()
     useEffect(() => {
         dispatch(fetchBooks());
     }, [dispatch]);
-    console.log(bookStore, "datas1");
+    const { Books } = bookStore;
+    console.log(Books, "datas1");
+    let regex = /[+-]?\d+(\.\d+)?/g;
+    const maxStars = 5;
+    const getStars = (star) => {
+        const nbrStart = star.match(regex)[0];
+        // const startPercentage = (nbrStart/maxStars) * 100;
+        // const startPercentageRounded = `${Math.round(startPercentage /10) * 10}%`
+        return Number(nbrStart);
+     }
     return (
         <div className="book-container">
             <h2>My books</h2>
+            <div className="books">
+                {
+                    Books?.filter(b => b.id <16).map(book => (
+                        <div className="book-item">
+                            <img src={book.imgUrl} />
+                            <div className="item-details">
+                                <StarRating star={getStars(book.review)}/>
+                                <p>{book.price}</p>
+                                <FaChevronCircleRight
+                                    color="blue"
+                                    size={15}
+                                    className="chevron"/>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
