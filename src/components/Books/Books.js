777032f690a142b8d0dd6801e005c423e7f9781f
benjamin kisenge <benkisenge03@gmail.com>
2022-10-14 12:45:36 +0200

    Import style file

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { useSelector} from 'react-redux';
 import { FaChevronCircleRight } from 'react-icons/fa';
 import './Books.css'
 import StarRating from "../StartRating/StartRating";
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 
 const Books = () => {
     const bookStore = useSelector((state) => state.books);
-    const dispatch = useDispatch();
     const { Books } = bookStore;
     console.log(Books, "datas1");
     let regex = /[+-]?\d+(\.\d+)?/g;
@@ -17,7 +16,6 @@ const Books = () => {
      }
     return (
         <div className="book-container">
-            <h2>My books</h2>
             <div className="books">
                 {
                     Books?.filter(b => b.id <16).map(book => (
@@ -28,13 +26,11 @@ const Books = () => {
                                 <p>{book.price}</p>
                                 <Link className={({ isActive }) => `link ${(isActive ? 'active' : '')}`} to={`/detail/${book.id}`}>
                                     <FaChevronCircleRight
-                                        color="blue"
                                         size={15}
                                         className="chevron"
                                         onClick={() => console.log("Clicke", book.id)}
                                     />
                                 </Link>
-
                             </div>
                         </div>
                     ))
