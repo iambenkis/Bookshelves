aea1f074b2a9b17e97b24bd8e488dbd3a918e66a
benjamin kisenge <benkisenge03@gmail.com>
2022-10-14 02:22:34 +0200

    Add  in the main file

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,25 +1,18 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchBooks } from "../../redux/bookReducer/reducer";
-import { AiOutlineStar } from 'react-icons/ai';
 import { FaChevronCircleRight } from 'react-icons/fa';
 import './Books.css'
 import StarRating from "../StartRating/StartRating";
+import { Link } from "react-router-dom";
 
 const Books = () => {
     const bookStore = useSelector((state) => state.books);
     const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchBooks());
-    }, [dispatch]);
     const { Books } = bookStore;
     console.log(Books, "datas1");
     let regex = /[+-]?\d+(\.\d+)?/g;
-    const maxStars = 5;
     const getStars = (star) => {
         const nbrStart = star.match(regex)[0];
-        // const startPercentage = (nbrStart/maxStars) * 100;
-        // const startPercentageRounded = `${Math.round(startPercentage /10) * 10}%`
         return Number(nbrStart);
      }
     return (
@@ -33,10 +26,15 @@ const Books = () => {
                             <div className="item-details">
                                 <StarRating star={getStars(book.review)}/>
                                 <p>{book.price}</p>
-                                <FaChevronCircleRight
-                                    color="blue"
-                                    size={15}
-                                    className="chevron"/>
+                                <Link className={({ isActive }) => `link ${(isActive ? 'active' : '')}`} to={`/detail/${book.id}`}>
+                                    <FaChevronCircleRight
+                                        color="blue"
+                                        size={15}
+                                        className="chevron"
+                                        onClick={() => console.log("Clicke", book.id)}
+                                    />
+                                </Link>
+
                             </div>
                         </div>
                     ))
