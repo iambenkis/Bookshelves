ea9829e69cf0ba797cf551b16df0c97851652d23
benjamin kisenge <benkisenge03@gmail.com>
2022-10-14 02:20:37 +0200

    Routes the components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   Routes,
   Route,
   Navigate,
 } from 'react-router-dom';
 import Home from './components/Home/Home';
-import axios from 'axios';
-
-
+import BookDetail from './components/BookDetails/BookDetails';
+import { fetchBooks ,getBooks } from './redux/bookReducer/reducer';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+    dispatch(getBooks());
+}, [dispatch]);
       return (
         <>
-            <Home />
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Navigate replace to="/home" />} />
+              <Route path="/detail/:id" element={<BookDetail />} />
+            </Routes>
         </>
       );
 };
