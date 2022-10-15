1bc50c147660ef9d3fa8b3098a6c0a0db7f48c99
benjamin kisenge <benkisenge03@gmail.com>
2022-10-15 22:37:08 +0200

    Add search feature

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -6,7 +6,7 @@ import './media.css';
 import { Link } from 'react-router-dom';
 import StarRating from '../StartRating/StartRating';
 
-const Books = () => {
+const Books = ({ searchItem }) => {
   const bookStore = useSelector((state) => state.books);
   const { Books } = bookStore;
   const regex = /[+-]?\d+(\.\d+)?/g;
@@ -18,22 +18,29 @@ const Books = () => {
     <div className="book-container">
       <div className="books">
         {
-                    Books?.filter((b) => b.id < 16).map((book, i) => (
-                      <div className="book-item" key={book.id}>
-                        <img src={book.imgUrl} alt="img" />
-                        <div className="item-details">
-                          <StarRating star={getStars(book.review)} id={i} />
-                          <p>{book.price}</p>
-                          <Link className={({ isActive }) => `link ${(isActive ? 'active' : '')}`} to={`/detail/${book.id}`}>
-                            <FaChevronCircleRight
-                              size={15}
-                              className="chevron"
-                            />
-                          </Link>
-                        </div>
-                      </div>
-                    ))
-                }
+          Books?.filter((b) => (b.id < 16)).filter((b) => {
+            if (searchItem === '') {
+              return b;
+            }
+            if (b.title.toLowerCase().includes(searchItem.toLowerCase())) {
+              return b;
+            }
+          }).map((book, i) => (
+            <div className="book-item" key={book.id}>
+              <img src={book.imgUrl} alt="img" />
+              <div className="item-details">
+                <StarRating star={getStars(book.review)} id={i} />
+                <p>{book.price}</p>
+                <Link className={({ isActive }) => `link ${(isActive ? 'active' : '')}`} to={`/detail/${book.id}`}>
+                  <FaChevronCircleRight
+                    size={15}
+                    className="chevron"
+                  />
+                </Link>
+              </div>
+            </div>
+          ))
+}
       </div>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,52 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMenuFold, AiOutlineSetting } from 'react-icons/ai';
 import { BiMicrophone, BiSearchAlt2 } from 'react-icons/bi';
 import Books from '../Books/Books';
 import './Home.css';
 import './media.css';
 
-const Home = () => (
-  <div className="container">
-    <div className="header">
-      <div className="left-side">
-        <AiOutlineMenuFold className="menu" />
-        <h3>My books</h3>
-      </div>
-      <div className="right-side">
-        <BiMicrophone className="micro" />
-        <AiOutlineSetting className="setting" />
-      </div>
-    </div>
-    <div className="navlink" />
-    <ul className="nav">
-      <li>Home</li>
-      <li>Books</li>
-      <li>About</li>
-    </ul>
-    <img
-      src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-      alt="bg"
-      className="bg"
-    />
-    <div className="band">
-      <p>BOOK WITH STARS AND PRICE</p>
-      <div>
-        <input type="text" placeholder="Search..." />
-        <div className="div-search">
-          <BiSearchAlt2 className="search" />
+const Home = () => {
+  const [searchItem, setSeachItem] = useState('');
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="left-side">
+          <AiOutlineMenuFold className="menu" />
+          <h3>My books</h3>
+        </div>
+        <div className="right-side">
+          <BiMicrophone className="micro" />
+          <AiOutlineSetting className="setting" />
         </div>
       </div>
+      <div className="navlink" />
+      <ul className="nav">
+        <li>Home</li>
+        <li>Books</li>
+        <li>About</li>
+      </ul>
+      <img
+        src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="bg"
+        className="bg"
+      />
+      <div className="band">
+        <p>BOOK WITH STARS AND PRICE</p>
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => {
+              setSeachItem(e.target.value);
+            }}
+          />
+          <div className="div-search">
+            <BiSearchAlt2 className="search" />
+          </div>
+        </div>
+      </div>
+      <div className="sort">
+        <select>
+          <option>Sort by</option>
+          <option>name</option>
+          <option>star</option>
+          <option>price</option>
+        </select>
+      </div>
+      <Books searchItem={searchItem} />
     </div>
-    <div className="sort">
-      <select>
-        <option>Sort by</option>
-        <option>name</option>
-        <option>star</option>
-        <option>price</option>
-      </select>
-    </div>
-    <Books />
-  </div>
-);
+  );
+};
 
 export default Home;
