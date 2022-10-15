72cf26fa1619afe2b703689e23013b1de783fc96
benjamin kisenge <benkisenge03@gmail.com>
2022-10-15 09:51:35 +0200

    Update UI

--- src/components/BookDetails/BookDetails.js
+++ src/components/BookDetails/BookDetails.js
@@ -6,6 +6,7 @@ import { FaChevronLeft } from 'react-icons/fa';
 import StarRating from '../StartRating/StartRating';
 import { getBooks } from '../../redux/bookReducer/reducer';
 import './Details.css';
+import './media.css';
 
 const BookDetail = () => {
   const bookStore = useSelector((state) => state.books);
@@ -34,17 +35,21 @@ const BookDetail = () => {
           <AiOutlineSetting className="setting" />
         </div>
       </div>
-      <img src={book?.imgUrl} alt="" />
-      <h3>{book?.title}</h3>
-      <div className="item-details">
-        <StarRating star={getStars(book?.review)} />
-        <p>{book?.price}</p>
+      <div className="detail">
+        <img src={book?.imgUrl} alt="" />
+        <div className="detail-description">
+          <h3>{book?.title}</h3>
+          <div className="item-details">
+            <StarRating star={getStars(book?.review)} />
+            <p>{book?.price}</p>
+          </div>
+          <p className="description">{book?.description}</p>
+          <span>
+            Source :
+            {book?.source}
+          </span>
+        </div>
       </div>
-      <p className="description">{book?.description}</p>
-      <span>
-        Source :
-        {book?.source}
-      </span>
     </div>
   );
 };
